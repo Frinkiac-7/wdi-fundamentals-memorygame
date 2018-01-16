9a550f7bba940cf9dbf2faf67eeb719b0d5b166c
Roberto Garcia <rgarcia@MacBook-Pro.local>
2018-01-16 09:05:14 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -5,19 +5,34 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry!  Try again!");
+		}
+	}	
+}
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cardId);
+	checkForMatch();
+
+//	console.log("User flipped " + cardsInPlay[1]);
+
+}
+
+
+flipCard(0);
+flipCard(2);
+
+
+/*
 var cardOne = cards[0];
 var cardTwo = cards[1];
 
 cardsInPlay.push(cardOne);
 cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry!  Try again!");
-	}
-}
+*/
